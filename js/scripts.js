7252bbc8d1ba37c1f69242073ef96a80db688679
rraneighh <ranemagno@gmail.com>
2021-03-08 00:06:26 +1300

    v4 work on nav

--- js/scripts.js
+++ js/scripts.js
@@ -24,21 +24,41 @@ $('#closeIcon').click(function(){
 //    -remove class for hidden
 // 2. hide other information
 
+// start of document element properties
+$('.synonym-list').show();
+$('.antonym-list').hide();
+$('.entymol-list').hide();
 
-$('.filter-antonym').click(function(){
-  $('.filter-antonym').addClass('show-filter');
-  $('.filter-synonym').removeClass('show-filter');
-  $('.filter-entymol').removeClass('show-filter');
-});
 
+// show synonyms BEGIN
 $('.filter-synonym').click(function(){
   $('.filter-synonym').addClass('show-filter');
   $('.filter-antonym').removeClass('show-filter');
   $('.filter-entymol').removeClass('show-filter');
+  $('.synonym-list').show();
+  $('.antonym-list').hide();
+  $('.entymol-list').hide();
 });
+// show synonyms ENDS
 
+// show antonyms BEGINS
+$('.filter-antonym').click(function(){
+  $('.filter-antonym').addClass('show-filter');
+  $('.filter-synonym').removeClass('show-filter, show-filter');
+  $('.filter-entymol').removeClass('show-filter');
+  $('.antonym-list').show();
+  $('.synonym-list').hide();
+  $('.entymol-list').hide();
+});
+// show antonyms ENDS
+
+// show entymology BEGINS
 $('.filter-entymol').click(function(){
   $('.filter-entymol').addClass('show-filter');
+  $('.filter-synonym').removeClass('show-filter, show-filter');
   $('.filter-antonym').removeClass('show-filter');
-  $('.filter-synonym').removeClass('show-filter');
+  $('.entymol-list').show();
+  $('.synonym-list').hide();
+  $('.antonym-list').hide();
 });
+// show entymology ENDS
