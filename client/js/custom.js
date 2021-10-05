1bc9f9d00948416782608af9598138ece02b6b6f
rraneighh <ranemagno@gmail.com>
2021-10-05 21:51:08 +1300

    added : showing definitions

--- client/js/custom.js
+++ client/js/custom.js
@@ -1,5 +1,99 @@
+// Dictionary Project - Rane Magno - github.com/rraneighh
 (function() {
+
+
   'use strict';
+
+
+// Initial Variables and Setup -------------------------------------------------
+  var word = document.querySelector('.word');
+  var pronounce = document.querySelector('.pronounce');
+  var definitionParent = document.querySelector('.cont-definition')
+  var definition = document.querySelector('.definition');
+  var body = document.querySelector('body');
+  var variationParent = document.querySelector('.variations')
+  var variation = document.querySelector('.variation')
+
+  body.onload = async () => {
+    let data = await getWordInfo('hello');
+    updateInfo(data)
+    changeInfo(data.meanings[0]);
+  }
+
+// Initial Variables and Setup -------------------------------------------------
+
+
+// Getting Data ----------------------------------------------------------------
+
+  let getWordInfo = async ( word ) => {
+    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    // without await it will run before the response and the promise will stay pending
+    if (!response.ok){
+      sorry(word)
+    }
+    let data = await response.json();
+    let wordInfo = await data[0];
+    // let update = await updateInfo(wordInfo);
+    return wordInfo
+  }
+
+  // Error Msg
+  let sorry = async ( word ) => {
+    console.log(`Sorry! Can't seem to find the word - `, word);
+  }
+
+// Getting Data ----------------------------------------------------------------
+
+
+// Updating & Change Page Info -------------------------------------------------
+
+  let updateInfo = async ( data ) => {
+    console.dir(data);
+
+    // Initial Setup
+    let wordData = data.word;
+    let phoneticText = data.phonetics[0].text;
+    let phoneticSound = data.phonetics[0].audio;
+    word.textContent = wordData;
+    pronounce.textContent = `/ ${phoneticText} /`;
+    changeInfo(data.meanings[0]);
+    variationParent.innerHTML = '';
+
+    for (const meaning of data.meanings){
+      let i = data.meanings.indexOf(meaning);
+      variationParent.innerHTML += `
+        <span class="variation">${i + 1}</span>
+      `;
+      let variationUpdated = await document.querySelectorAll('.variation');
+      variationUpdated.forEach((variation) => {
+        variation.addEventListener('click', () => {
+          changeInfo(data.meanings[variation.textContent - 1])
+        })
+      })
+    }
+
+  // end - updateInfo
+  }
+
+  let changeInfo = ( def ) => {
+    console.log(def);
+    let typeData = def.partOfSpeech
+    definitionParent.innerHTML = `<p class="definition">${typeData} - </p>`;
+
+    let allDef = def.definitions;
+    for (const def of allDef){
+      let i = allDef.indexOf(def);
+      // converting from numbers to corresponding letters
+      let defCount = String.fromCharCode(97 + i);
+      definitionParent.innerHTML += `<p class="definition">${defCount}. ${def.definition}</p>`;
+    }
+
+  }
+
+// Updating & Change Page Info -------------------------------------------------
+
+
+// Synonyms and Antonyms -------------------------------------------------------
 
   let extras = document.querySelectorAll('.extra');
   let extraInfo = document.querySelector('.extra-info')
@@ -19,55 +113,28 @@
     })
   })
 
-  // fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/hello`)
-  // .then(data => { return data.json() })
-  // .then(res => {console.log(res[0].word)})
-  // .catch(err => {console.log(err)})
+// Synonyms and Antonyms -------------------------------------------------------
 
-  let getWordInfo = async ( word ) => {
-    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-    // without await it will run before the response and the promise will stay pending
-    if (!response.ok){
-      sorry(word)
-    }
-    let data = await response.json();
-    let wordInfo = await data[0];
-    let update = await updateInfo(wordInfo);
-    console.log(wordInfo);
-    return wordInfo
-  }
 
-  function updateInfo( data ) {
-    word.textContent = data.word;
-    pronounce.textContent = `/${data.phonetic}/`;
-
-  }
-
-  let sorry = async ( word ) => {
-    console.log(`Sorry! Can't seem to find the word - `, word);
-  }
+// Input Handler ---------------------------------------------------------------
 
   // Get the input field
   var input = document.querySelector("#searchInput");
 
   // Execute a function when the user releases a key on the keyboard
-  input.addEventListener("keyup", function(event) {
+  input.addEventListener('keyup', async (event) => {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
       // Cancel the default action, if needed
       // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
       event.preventDefault();
-      getWordInfo(input.value)
+      let wordInfo = await getWordInfo(input.value);
+      updateInfo(wordInfo);
     }
   });
 
-  // Result Variables
-  var word = document.querySelector('.word');
-  var pronounce = document.querySelector('.pronounce');
-  var definition = document.querySelector('.definition');
+// Input Handler ---------------------------------------------------------------
 
 
-
-
-
+// END OF CODE
 }())
